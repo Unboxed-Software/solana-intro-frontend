4a8b1d240186ea9386f6b5dee4677f66c6dd1964
James Pacheco <jamesrp13@gmail.com>
2022-04-21 13:26:53 -0600

    Add solution code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,26 @@
 import type { NextPage } from 'next'
 import { useState } from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
+import * as Web3 from '@solana/web3.js'
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
 
   const addressSubmittedHandler = (address: string) => {
-    setAddress(address)
-    setBalance(1000)
+    try {
+      setAddress(address)
+      const key = new Web3.PublicKey(address)
+      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+      connection.getBalance(key).then(balance => {
+        setBalance(balance / Web3.LAMPORTS_PER_SOL)
+      })
+    } catch (error) {
+      setAddress('')
+      setBalance(0)
+      alert(error)
+    }
   }
 
   return (
