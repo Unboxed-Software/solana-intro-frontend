da6c4b697328822796d091346ef808285e32b896
yoctoyu <yoctoyu@gmail.com>
2022-11-14 11:29:20 +0300

    First Challenge

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import * as Web3 from '@solana/web3.js'
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [isExecutable, setIsExecutable] = useState(false);
 
   const addressSubmittedHandler = (address: string) => {
     try {
@@ -15,6 +16,9 @@ const Home: NextPage = () => {
       const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
       connection.getBalance(key).then(balance => {
         setBalance(balance / Web3.LAMPORTS_PER_SOL)
+      })
+      connection.getAccountInfo(key).then(info => {
+        setIsExecutable(info?.executable ?? false);
       })
     } catch (error) {
       setAddress('')
@@ -32,6 +36,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable? ${isExecutable ? 'Yep' : 'Nope'}`}</p>
       </header>
     </div>
   )
